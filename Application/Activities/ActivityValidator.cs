4000116bc01a4763df9c71495aee68e3848543f8
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-10-19 16:04:24 +0500

    working with fluent validator c# package

---
+++ Application/Activities/ActivityValidator.cs
@@ -0,0 +1,19 @@
+using Domain;
+using FluentValidation;
+
+namespace Application.Activities
+{
+    public class ActivityValidator : AbstractValidator<Activity>
+    {
+        public ActivityValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+        }
+    }
+}
