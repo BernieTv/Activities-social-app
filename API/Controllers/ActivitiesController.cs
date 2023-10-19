e5fd80bfbb0e0ba7dfa2492af66aa30ee270f924
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-10-19 17:38:22 +0500

    validating activities delete function data

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -37,9 +37,7 @@ namespace API.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            await Mediator.Send(new Delete.Command { Id = id });
-
-            return Ok();
+            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
