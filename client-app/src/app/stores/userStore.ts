23a05e053ee5b4866212df6f818fbdc9dd15c736
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-10-25 15:58:19 +0500

    implemented login functionality on FE

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,7 +1,9 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 
 import { User, UserFormValues } from '../models/user';
 import agent from '../api/agent';
+import { store } from './store';
+import { router } from '../router/Routes';
 
 export default class UserStore {
   user: User | null = null;
@@ -16,7 +18,17 @@ export default class UserStore {
 
   login = async (credentials: UserFormValues) => {
     const user = await agent.Account.login(credentials);
+    store.commonStore.setToken(user.token);
+    runInAction(() => (this.user = user));
 
-    console.log(user);
+    router.navigate('/activities');
+  };
+
+  logout = () => {
+    store.commonStore.setToken(null);
+    localStorage.removeItem('jwt');
+    this.user = null;
+
+    router.navigate('/');
   };
 }
--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,20 +1,38 @@
-import { Form, Formik } from 'formik';
-import MyTextInput from '../../app/common/form/MyTextInput';
-import { Button } from 'semantic-ui-react';
+import { ErrorMessage, Form, Formik } from 'formik';
+import { observer } from 'mobx-react-lite';
+import { Button, Label } from 'semantic-ui-react';
 
-const LoginForm = () => {
+import MyTextInput from '../../app/common/form/MyTextInput';
+import { useStore } from '../../app/stores/store';
+
+const LoginForm = observer(() => {
+  const {
+    userStore: { login },
+  } = useStore();
+
   return (
-    <Formik initialValues={{ email: '', password: '' }} onSubmit={(values) => console.log(values)}>
-      {({ handleSubmit }) => (
+    <Formik
+      initialValues={{ email: '', password: '', error: null }}
+      onSubmit={(values, { setErrors }) =>
+        login(values).catch(() => setErrors({ error: 'Invalid email or password' }))
+      }>
+      {({ handleSubmit, isSubmitting, errors }) => (
         <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
           <MyTextInput placeholder="Email" name="email" />
           <MyTextInput placeholder="Password" name="password" type="password" />
 
-          <Button positive content="Login" type="submit" fluid />
+          <ErrorMessage
+            name="error"
+            render={() => (
+              <Label style={{ marginBottom: 10 }} basic color="red" content={errors.error} />
+            )}
+          />
+
+          <Button positive content="Login" type="submit" fluid loading={isSubmitting} />
         </Form>
       )}
     </Formik>
   );
-};
+});
 
 export default LoginForm;
