9a59332b228016e4cfae63697858f5d6419eaeea
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-10-26 18:15:45 +0500

    updating attendees in mobx store

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -4,6 +4,7 @@ import { format } from 'date-fns';
 import { Button, Header, Item, Segment, Image } from 'semantic-ui-react';
 
 import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
 
 const activityImageStyle = {
   filter: 'brightness(30%)',
@@ -23,6 +24,10 @@ interface Props {
 }
 
 const ActivityDetailedHeader = observer(({ activity }: Props) => {
+  const {
+    activityStore: { updateAttendance, loading },
+  } = useStore();
+
   return (
     <Segment.Group>
       <Segment basic attached="top" style={{ padding: '0' }}>
@@ -58,9 +63,13 @@ const ActivityDetailedHeader = observer(({ activity }: Props) => {
             Manage Event
           </Button>
         ) : activity.isGoing ? (
-          <Button>Cancel attendance</Button>
+          <Button loading={loading} onClick={updateAttendance}>
+            Cancel attendance
+          </Button>
         ) : (
-          <Button color="teal">Join Activity</Button>
+          <Button loading={loading} color="teal" onClick={updateAttendance}>
+            Join Activity
+          </Button>
         )}
       </Segment>
     </Segment.Group>
