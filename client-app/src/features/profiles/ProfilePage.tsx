b820f259dcf1509c7da56eb09e06d7c76c87aaa7
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-11-12 16:07:36 +0500

    created ProfileHeader component

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,5 +1,15 @@
+import { Grid } from 'semantic-ui-react';
+
+import ProfileHeader from './ProfileHeader';
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <Grid>
+      <Grid.Column width={16}>
+        <ProfileHeader />
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default ProfilePage;
