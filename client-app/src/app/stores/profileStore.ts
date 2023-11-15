c0ed4d5246da8b71ac57a8ed4740f867677233f3
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-11-15 21:53:08 +0500

    worked on set main photo logic on FE

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,6 +1,6 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 
-import { Profile } from '../models/profile';
+import { Photo, Profile } from '../models/profile';
 import agent from '../api/agent';
 import { store } from './store';
 
@@ -8,6 +8,7 @@ export default class ProfileStore {
   profile: Profile | null = null;
   loadingProfile = false;
   uploading = false;
+  loading = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -60,8 +61,34 @@ export default class ProfileStore {
       });
     } catch (error) {
       console.log(error);
+
       runInAction(() => {
         this.uploading = false;
+      });
+    }
+  };
+
+  setMainPhoto = async (photo: Photo) => {
+    this.loading = true;
+
+    try {
+      await agent.Profiles.setMainPhoto(photo.id);
+      store.userStore.setImage(photo.url);
+
+      runInAction(() => {
+        if (this.profile && this.profile.photos) {
+          this.profile.photos.find((p) => p.isMain)!.isMain = false;
+          this.profile.photos.find((p) => p.id === photo.id)!.isMain = true;
+          this.profile.image = photo.url;
+
+          this.loading = false;
+        }
+      });
+    } catch (error) {
+      console.log(error);
+
+      runInAction(() => {
+        this.loading = false;
       });
     }
   };
--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Button, Card, Grid, Header, Image, Tab } from 'semantic-ui-react';
 
-import { Profile } from '../../app/models/profile';
+import { Photo, Profile } from '../../app/models/profile';
 import { useStore } from '../../app/stores/store';
 import PhotoUploadWidget from '../../app/common/imageUpload/PhotoUploadWidget';
 
@@ -12,12 +12,18 @@ interface Props {
 
 const ProfilePhotos = observer(({ profile }: Props) => {
   const {
-    profileStore: { isCurrentUser, uploadPhoto, uploading },
+    profileStore: { isCurrentUser, uploadPhoto, uploading, loading, setMainPhoto },
   } = useStore();
   const [addPhotoMode, setAddPhotoMode] = useState(false);
+  const [target, setTarget] = useState('');
 
   const handlePhotoUpload = (file: Blob) => {
     uploadPhoto(file).then(() => setAddPhotoMode(false));
+  };
+
+  const handleSetMainPhoto = (photo: Photo, e: SyntheticEvent<HTMLButtonElement>) => {
+    setTarget(e.currentTarget.name);
+    setMainPhoto(photo);
   };
 
   return (
@@ -44,6 +50,22 @@ const ProfilePhotos = observer(({ profile }: Props) => {
               {profile.photos?.map((photo) => (
                 <Card key={photo.id}>
                   <Image src={photo.url} />
+
+                  {isCurrentUser ? (
+                    <Button.Group fluid widths={2}>
+                      <Button
+                        basic
+                        color="green"
+                        content="Main"
+                        name={photo.id}
+                        disabled={photo.isMain}
+                        loading={target === photo.id && loading}
+                        onClick={(e) => handleSetMainPhoto(photo, e)}
+                      />
+
+                      <Button basic color="red" icon="trash" />
+                    </Button.Group>
+                  ) : null}
                 </Card>
               ))}
             </Card.Group>
