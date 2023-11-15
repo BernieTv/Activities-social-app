5d471309f3361ff5b4282a2624ebb66d35069332
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-11-15 15:49:07 +0500

    working on add photo functionality

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,24 +1,50 @@
+import { useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Card, Header, Image, Tab } from 'semantic-ui-react';
+import { Button, Card, Grid, Header, Image, Tab } from 'semantic-ui-react';
 
 import { Profile } from '../../app/models/profile';
+import { useStore } from '../../app/stores/store';
 
 interface Props {
   profile: Profile;
 }
 
 const ProfilePhotos = observer(({ profile }: Props) => {
+  const {
+    profileStore: { isCurrentUser },
+  } = useStore();
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+
   return (
     <Tab.Pane>
-      <Header icon="image" content="Photos" />
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated="left" icon="image" content="Photos" />
 
-      <Card.Group>
-        {profile.photos?.map((photo) => (
-          <Card key={photo.id}>
-            <Image src={photo.url} />
-          </Card>
-        ))}
-      </Card.Group>
+          {isCurrentUser ? (
+            <Button
+              floated="right"
+              basic
+              content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+              onClick={() => setAddPhotoMode((prev) => !prev)}
+            />
+          ) : null}
+        </Grid.Column>
+
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <p>Photo widget goes here</p>
+          ) : (
+            <Card.Group>
+              {profile.photos?.map((photo) => (
+                <Card key={photo.id}>
+                  <Image src={photo.url} />
+                </Card>
+              ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
     </Tab.Pane>
   );
 });
