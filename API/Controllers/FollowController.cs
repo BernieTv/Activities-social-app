9b64470f38190c03e0ee459d800994935bb7e174
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-11-16 20:05:03 +0500

    getting list of followers by username and predicate

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -12,5 +12,13 @@ namespace API.Controllers
                 await Mediator.Send(new FollowToggle.Command { TargetUsername = username })
             );
         }
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowings(string username, string predicate)
+        {
+            return HandleResult(
+                await Mediator.Send(new List.Query { Username = username, Predicate = predicate })
+            );
+        }
     }
 }
