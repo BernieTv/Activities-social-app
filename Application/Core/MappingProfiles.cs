c59ed0fb3313704d1331a5f0ee5df5220152abc1
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-12-03 14:30:52 +0500

    connecting followers data to app user json responses

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -9,6 +9,8 @@ namespace Application.Core
     {
         public MappingProfiles()
         {
+            string currentUsername = null;
+
             CreateMap<Activity, Activity>();
 
             CreateMap<Activity, ActivityDto>()
@@ -24,6 +26,16 @@ namespace Application.Core
                 .ForMember(
                     d => d.Image,
                     o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url)
+                )
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.AppUser.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.AppUser.Followings.Count))
+                .ForMember(
+                    d => d.Following,
+                    o =>
+                        o.MapFrom(
+                            s =>
+                                s.AppUser.Followers.Any(x => x.Observer.UserName == currentUsername)
+                        )
                 );
 
             CreateMap<AppUser, Profiles.Profile>()
@@ -32,7 +44,12 @@ namespace Application.Core
                     o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url)
                 )
                 .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
-                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count))
+                .ForMember(
+                    d => d.Following,
+                    o =>
+                        o.MapFrom(s => s.Followers.Any(x => x.Observer.UserName == currentUsername))
+                );
 
             CreateMap<Comment, CommentDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
