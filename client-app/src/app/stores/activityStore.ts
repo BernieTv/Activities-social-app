d80143004ac9c75ad0d91d9c70a704568af5cf53
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2023-12-05 23:51:14 +0500

    added pagination parameters

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,7 +5,7 @@ import { Activity, ActivityFormValues } from '../models/activity';
 import agent from '../api/agent';
 import { store } from './store';
 import { Profile } from '../models/profile';
-import { Pagination } from '../models/pagination';
+import { Pagination, PagingParams } from '../models/pagination';
 
 export default class ActivityStore {
   activityRegistry = new Map<string, Activity>();
@@ -13,9 +13,23 @@ export default class ActivityStore {
   loading = false;
   loadingInitial = false;
   pagination: Pagination | null = null;
+  pagingParams = new PagingParams();
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  setPagingParams = (pagingParams: PagingParams) => {
+    this.pagingParams = pagingParams;
+  };
+
+  get axiosParams() {
+    const params = new URLSearchParams();
+
+    params.append('pageNumber', this.pagingParams.pageNumber.toString());
+    params.append('pageSize', this.pagingParams.pageSize.toString());
+
+    return params;
   }
 
   get activitiesByDate() {
@@ -40,7 +54,7 @@ export default class ActivityStore {
     this.setLoadingInitial(true);
 
     try {
-      const result = await agent.Activities.list();
+      const result = await agent.Activities.list(this.axiosParams);
 
       runInAction(() => {
         result.data.forEach((activity) => {
