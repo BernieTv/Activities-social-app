436b62ac294b437e688b917e7c00803a2060456c
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2024-01-08 14:41:36 +0500

    added a verify email component

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -10,6 +10,7 @@ import ServerError from '../../features/errors/ServerError';
 import ProfilePage from '../../features/profiles/ProfilePage';
 import RequireAuth from './RequireAuth';
 import RegisterSuccess from '../../features/users/RegisterSuccess';
+import ConfirmEmail from '../../features/users/ConfirmEmail';
 
 export const routes: RouteObject[] = [
   {
@@ -30,6 +31,7 @@ export const routes: RouteObject[] = [
       },
 
       { path: 'account/registerSuccess', element: <RegisterSuccess /> },
+      { path: 'account/verifyEmail', element: <ConfirmEmail /> },
 
       { path: 'not-found', element: <NotFound /> },
       { path: 'server-error', element: <ServerError /> },
--- client-app/src/features/users/RegisterSuccess.tsx
+++ client-app/src/features/users/RegisterSuccess.tsx
@@ -16,6 +16,7 @@ const RegisterSuccess = () => {
         console.log(error);
       });
   };
+
   return (
     <Segment placeholder textAlign="center">
       <Header icon color="green">
